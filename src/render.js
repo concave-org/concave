3a8fbbd4edb15c3fc0ea1470a2328496ee8da5eb
Mats Pfeiffer <mats.pfeiffer@googlemail.com>
2019-01-28 20:11:22 +0100

    fix(render): Only add event listener when element exists.

--- src/render.js
+++ src/render.js
@@ -22,15 +22,18 @@ const createRenderer = parent => {
       case sa:
         Object.keys(action.value).forEach(id => {
           let el = parent.querySelector(`#${id}`)
-          Object.keys(action.value[id]).forEach(attr => {
-            let event = attr.startsWith('on') ? attr.substring(2) : null
-            if (event && typeof action.value[id][attr] === 'function') {
-              el.addEventListener(event, event => {
-                let result = action.value[id][attr](event)
-                if (typeof result === 'object') dispatch(result)
-              })
-            } else el.setAttribute(attr, action.value[id][attr])
-          })
+          // only add event listener when element exists
+          if (el) {
+            Object.keys(action.value[id]).forEach(attr => {
+              let event = attr.startsWith('on') ? attr.substring(2) : null
+              if (event && typeof action.value[id][attr] === 'function') {
+                el.addEventListener(event, event => {
+                  let result = action.value[id][attr](event)
+                  if (typeof result === 'object') dispatch(result)
+                })
+              } else el.setAttribute(attr, action.value[id][attr])
+            })
+          }
         })
         break
     }

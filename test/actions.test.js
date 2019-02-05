2695c2a53dc64123a6f2ebb4f37d500db9f1af3f
Mats Pfeiffer <mats.pfeiffer@googlemail.com>
2019-02-05 19:25:13 +0100

    test(actions): Fix tests after breaking changes.

--- test/actions.test.js
+++ test/actions.test.js
@@ -1,20 +1,16 @@
 const req = require('esm')(module, { mode: 'auto' })
-const {
-  aState,
-  aRouteG,
-  aRouteN,
-  pc,
-  rb,
-  ac,
-  rc
-} = req('../src/actions')
+const { actions } = req('../src/actions')
 
-test('actions are numbers', () => {
-  expect(typeof aState).toEqual('number')
-  expect(typeof aRouteG).toEqual('number')
-  expect(typeof aRouteN).toEqual('number')
-  expect(typeof pc).toEqual('number')
-  expect(typeof rb).toEqual('number')
-  expect(typeof ac).toEqual('number')
-  expect(typeof rc).toEqual('number')
+test('actions are an object', () => {
+  expect(typeof actions).toEqual('object')
+})
+
+test('actions are strings', () => {
+  expect(typeof actions.state).toEqual('string')
+  expect(typeof actions.routeTo).toEqual('string')
+  expect(typeof actions.routeNew).toEqual('string')
+  expect(typeof actions.pipelineChange).toEqual('string')
+  expect(typeof actions.routeBack).toEqual('string')
+  expect(typeof actions.appendComponent).toEqual('string')
+  expect(typeof actions.replaceComponent).toEqual('string')
 })

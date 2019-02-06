5cfe44793814bd4e34cd918a505a415026f4a0d6
Mats Pfeiffer <mats.pfeiffer@googlemail.com>
2019-02-06 14:08:21 +0100

    revert(html): Revert regex changes.

--- src/html.js
+++ src/html.js
@@ -2,9 +2,6 @@ const h = (literals, ...substitutions) => {
   // join arrays for easier usage in app e.g. list rendering
   return String
     .raw(literals, ...substitutions.map(x => Array.isArray(x) ? x.join('') : x))
-    .replace(/>\n+/g, '>')
-    .replace(/\s+</g, '<')
-    .replace(/>\s+/g, '>')
     .replace(/\n\s+/g, '')
 }
 

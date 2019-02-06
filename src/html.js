015033e8bb6910ad819c992139c6ebf73a13f4a1
Mats Pfeiffer <mats.pfeiffer@googlemail.com>
2019-02-06 12:28:19 +0100

    feat(html): Better template compression.

--- src/html.js
+++ src/html.js
@@ -2,7 +2,7 @@ const h = (literals, ...substitutions) => {
   // join arrays for easier usage in app e.g. list rendering
   return String
     .raw(literals, ...substitutions.map(x => Array.isArray(x) ? x.join('') : x))
-    .replace(/\n\s+/g, '')
+    .replace(/\s+\n/g, '')
 }
 
 export {

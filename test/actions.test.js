37317a5053adb3c97e93ad78e6159504aa2f8a44
Mats Pfeiffer <mats.pfeiffer@googlemail.com>
2019-02-14 12:57:07 +0100

    test(actions): Add missing action.

--- test/actions.test.js
+++ test/actions.test.js
@@ -1,16 +1,17 @@
 const req = require('esm')(module, { mode: 'auto' })
 const { actions } = req('../src/actions')
 
-test('actions are an object', () => {
+test('module exports actions as object', () => {
   expect(typeof actions).toEqual('object')
 })
 
 test('actions are strings', () => {
   expect(typeof actions.state).toEqual('string')
   expect(typeof actions.routeTo).toEqual('string')
+  expect(typeof actions.routeBack).toEqual('string')
   expect(typeof actions.routeNew).toEqual('string')
   expect(typeof actions.pipelineChange).toEqual('string')
-  expect(typeof actions.routeBack).toEqual('string')
   expect(typeof actions.appendComponent).toEqual('string')
   expect(typeof actions.replaceComponent).toEqual('string')
+  expect(typeof actions.setAttributes).toEqual('string')
 })

afcbb662b89ac44abf1ac67fde9948654ab0a1a2
Mats Pfeiffer <mats.pfeiffer@googlemail.com>
2019-02-14 12:45:29 +0100

    refactor(component): Dispatching render actions all together.

--- src/component.js
+++ src/component.js
@@ -9,16 +9,15 @@ const createComponent = (renderFn, props = {}) => {
     switch (action.type) {
       case actions.state:
         let { h, attrs } = renderFn(action.value, props)
-        let template = document.createElement('template')
         template.innerHTML = h
         let el = template.content.firstChild
         let renderAction = !prevEl ? actions.appendComponent : !prevEl.isEqualNode(el) ? actions.replaceComponent : null
         if (renderAction) {
-          dispatch({ type: renderAction, value: { el, prevEl } })
-          // static attributes
-          if (props._attrs) dispatch({ type: actions.setAttributes, value: props._attrs })
-          // dynamic attributes - only known at RUNTIME!
-          if (attrs) dispatch({ type: actions.setAttributes, value: attrs })
+          let toDispatch = []
+          toDispatch.push({ type: renderAction, value: { el, prevEl } })
+          if (props._attrs) toDispatch.push({ type: actions.setAttributes, value: props._attrs })
+          if (attrs) toDispatch.push({ type: actions.setAttributes, value: attrs })
+          dispatch(...toDispatch)
         }
         prevEl = el
         break

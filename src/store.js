2225292fd777a51a7fb393bcd1c6f649a61e13ec
Mats Pfeiffer <mats.pfeiffer@googlemail.com>
2019-04-30 13:21:22 +0200

    fix(store): use real deep copy function

--- src/store.js
+++ src/store.js
@@ -1,4 +1,5 @@
 import { actions } from './actions'
+import { deepCopy } from './utils'
 
 const createStore = stateReducer => {
   let state
@@ -7,22 +8,22 @@ const createStore = stateReducer => {
   return (action, dispatch) => {
     switch (action.type) {
       case actions.state:
-        if (!initialState) initialState = { ...action.value }
-        state = { ...action.value }
+        if (!initialState) initialState = deepCopy(action.value)
+        state = deepCopy(action.value)
         window.history.replaceState(state, null, document.location.href)
         break
       case actions.pipelineChange:
         // needed to reset the state with a routeBack action
-        if (action.value.state) state = { ...action.value.state }
-        state._route = { ...action.value.route }
+        if (action.value.state) state = deepCopy(action.value.state)
+        state._route = deepCopy(action.value.route)
         dispatch({ type: actions.state, value: state })
         break
       case actions.routeTo:
-        state = { ...initialState }
+        state = deepCopy(initialState)
         window.history.pushState(state, null, document.location.origin + action.value)
         break
     }
-    stateReducer({ ...state }, action, dispatch)
+    stateReducer(deepCopy(state), action, dispatch)
     return action
   }
 }
